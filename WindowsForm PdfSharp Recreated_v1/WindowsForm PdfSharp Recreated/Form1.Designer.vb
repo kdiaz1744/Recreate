c6f18eaa4a9220772d8c5e572d5b04c25edf6af4
Kevin Diaz <kdiaz1744@hotmail.com>
2024-07-26 20:11:32 -0400

    Add files via upload

---
+++ WindowsForm PdfSharp Recreated_v1/WindowsForm PdfSharp Recreated/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerateCertificate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGenerateCertificate
+        '
+        Me.btnGenerateCertificate.Location = New System.Drawing.Point(104, 54)
+        Me.btnGenerateCertificate.Name = "btnGenerateCertificate"
+        Me.btnGenerateCertificate.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerateCertificate.TabIndex = 0
+        Me.btnGenerateCertificate.Text = "Recreate"
+        Me.btnGenerateCertificate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnGenerateCertificate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGenerateCertificate As Button
+End Class
---
+++ WindowsForm PdfSharp Recreated_v1/WindowsForm PdfSharp Recreated/Form1.vb
@@ -0,0 +1,50 @@
+﻿
+Imports MigraDoc.DocumentObjectModel
+Imports MigraDoc.Rendering
+
+Public Class Form1
+    Private Sub btnGenerateCertificate_Click(sender As Object, e As EventArgs) Handles btnGenerateCertificate.Click
+        Dim regDate As Date = Date.Now()
+        Dim strDate As String = regDate.ToString("MMddyyyy") & "_" & regDate.ToString("HHmm")
+        'Variables fot path and filename 
+        Dim filename As String
+        Dim folderBrowser As New FolderBrowserDialog
+
+        Try
+
+            filename = "ReporteVal_Carefusion_" & strDate & ".pdf"
+
+            folderBrowser.SelectedPath = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+            folderBrowser.Description = "Select a destination folder for the Certificate of Compliance."
+            If Not folderBrowser.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                MsgBox("Error, a folder must be selected to save the Certification.", MsgBoxStyle.Exclamation, "Test Report")
+                Exit Sub
+            End If
+
+            MsgBox("EL folder seleccionado es:" & folderBrowser.SelectedPath)
+
+            'Create a object certificate
+            Dim certificate As Certificate = New Certificate("101010", "Revision 1010", "Form Rev")
+
+            Dim document As Document = certificate.CreateDocument()
+            document.UseCmykColor = True
+            'Const unicode As Boolean = False
+
+            'Const embedding As PdfFontEmbedding = PdfFontEmbedding.Always
+
+            Dim pdfRenderer As PdfDocumentRenderer = New PdfDocumentRenderer(True)
+
+            pdfRenderer.Document = document
+
+            pdfRenderer.RenderDocument()
+
+
+            pdfRenderer.PdfDocument.Save(folderBrowser.SelectedPath & "\" & filename)
+            MsgBox("The Certified was saved as: " & folderBrowser.SelectedPath & "\" & filename)
+            Process.Start(folderBrowser.SelectedPath & "\" & filename)
+        Catch ex As Exception
+            MsgBox("Error Migradosc: " & vbCrLf & ex.Message, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+End Class
